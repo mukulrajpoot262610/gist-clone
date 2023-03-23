feff5f669d9ea60b352be6afb2343e30f0640c74
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2023-03-23 09:23:30 +0530

    feat: theme system

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center flex-col">
       <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
-      <h1 className="text-3xl lg:text-7xl font-bold text-center">GitHub Gist Clone</h1>
+      <h1 className="text-3xl lg:text-7xl font-bold text-center font-mono">GitHub Gist Clone</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#2D333B',
+        light: '#22272E',
+        border: "#444C56",
+        button: {
+          secondary: '#373E47',
+          primary: '#347D39'
+        },
+        text: {
+          blue: "#3282F5",
+          white: '#CDD9E5',
+          grey: '#768390'
+        },
+      },
+      fontFamily: {
+        monospace: 'Space Mono'
+      }
+    },
   },
   plugins: [],
 }
