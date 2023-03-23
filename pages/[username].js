10bd2c529cf8052c0bb27a3f2e2db2958f591723
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2023-03-23 22:51:19 +0530

    ADD: profile ui right part

--- pages/[username].js
+++ pages/[username].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { BiLink } from 'react-icons/bi';
-import { BsTwitter } from 'react-icons/bs';
+import { BsCodeSquare, BsTwitter } from 'react-icons/bs';
 import { FiUsers } from 'react-icons/fi';
 import { GoLocation } from 'react-icons/go';
 
@@ -12,6 +12,7 @@ const Profile = () => {
     return (
         <div className="mx-auto max-w-7xl h-full w-full p-7">
             <div className="grid grid-cols-12 w-full">
+
                 <div className="col-span-3 p-3">
                     <div className="relative">
                         <img src="https://avatars.githubusercontent.com/u/73209159?v=4" className="rounded-full border-border-primary border" />
@@ -51,10 +52,33 @@ const Profile = () => {
                             <h3 className="font-light text-text-secondary tracking-tight">@mukul_eth</h3>
                         </div>
 
-                        <button className="btn btn-sm capitalize w-full mt-4 bg-dark text-text-secondary border-border-primary text-sm hover:bg-dark">View GitHub Profile</button>
+                        <button className="btn btn-sm capitalize w-full mt-4 bg-button-secondary text-text-secondary border-border-primary text-sm hover:bg-button-secondary">View GitHub Profile</button>
                     </div>
                 </div>
-                <div className="col-span-9 p-3"></div>
+
+                <div className="col-span-9 p-3">
+                    <div className="flex justify-between items-center">
+                        <div className="tabs text-text-secondary">
+                            <a className={`tab-bordered border-border-orange`}>
+                                <div className='flex items-center gap-2 m-1 mx-0 rounded-lg hover:bg-dark p-1 cursor-pointer text-sm'>
+                                    <BsCodeSquare className='font-bold' />
+                                    <h3 className="tracking-tight">All Gists</h3>
+                                    <h3 className="bg-button-secondary flex justify-center items-center rounded-full w-5 h-5 text-xs font-bold">3</h3>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div className="border flex justify-center items-center border-border-primary rounded-lg pl-3 bg-button-secondary text-xs font-medium">
+                            <h2>Type: </h2>
+                            <select className="select select-sm bg-button-secondary text-text-secondary border-0 max-w-xs text-xs">
+                                <option selected>All</option>
+                                <option>Public</option>
+                                <option>Private</option>
+                            </select>
+                        </div>
+                    </div>
+                    <hr className="border-border-primary" />
+                </div>
             </div>
         </div>
     )
